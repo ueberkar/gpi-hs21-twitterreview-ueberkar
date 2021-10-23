a357acc6bec63a2dd9d2a6f61d9e63bb2d8d7345
Karin Uebersax <ueberkar@students.zhaw.ch>
2021-10-23 11:25:28 +0200

    Semesterwoche 5 abgeschlossen

---
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/ldap/OrgUnitRepository.java
@@ -0,0 +1,6 @@
+package ch.zhaw.gpi.twitterreviewprocessapplication.ldap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrgUnitRepository extends JpaRepository<OrgUnit, String>{
+    
+}
