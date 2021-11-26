6a1088223d2d965399ba421a94a28173727a29c5
Karin Uebersax <ueberkar@students.zhaw.ch>
2021-11-26 18:02:37 +0100

    Semesterwoche 10 abgeschlossen

---
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/SendScheduledTweetDelegate.java
@@ -0,0 +1,24 @@
+package ch.zhaw.gpi.twitterreviewprocessapplication;
+
+import java.util.Date;
+import javax.inject.Named;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+
+@Named("sendTweetAdapter")
+public class SendScheduledTweetDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        String instanceStarted = " (Instance started: " + execution.getVariable("instanceStarted") + ")";
+        System.out.println(new Date().toString() + ": Delegate Execution started for: " +
+        execution.getProcessInstanceId() + instanceStarted);
+        Thread.sleep(120000);
+        System.out.println(new Date().toString() + ": Delegate Execution ended for: " +
+        execution.getProcessInstanceId() + instanceStarted);
+
+    }
+    
+}
